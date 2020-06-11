fd664e6937f1ceb0e8b560d2f2c9dc2a55e3539c
Hardik Munjal <hardik.munjal@hitachiconsulting.com>
2020-06-11 16:58:52 +0530

    Added assignment 3

--- getting_started_with_go/week2/assignment3.go
+++ getting_started_with_go/week2/assignment3.go
@@ -1,1 +1,30 @@
 package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func find_chars() {
+	var x string
+	fmt.Println("Please Enter the string: ")
+	fmt.Scan(&x)
+	x = strings.ToLower(x)
+	i := strings.Index(x, "i")
+	n := strings.Index(x, "n")
+	a := strings.Index(x, "a")
+	switch {
+	case i != 0:
+		fmt.Printf("Not Found!")
+	case n != len(x)-1:
+		fmt.Printf("Not Found!")
+	case a == -1:
+		fmt.Printf("Not Found!")
+	default:
+		fmt.Printf("Found!")
+	}
+}
+
+func main() {
+	find_chars()
+}
